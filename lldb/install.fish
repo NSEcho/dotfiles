661b96eee67ce26ceceef79a7e84a8bbb33ce0a3
lateralusd <xdaemonx@protonmail.ch>
2023-03-02 23:54:25 +0100

    add nasm and fix lldb install path

--- lldb/install.fish
+++ lldb/install.fish
@@ -5,7 +5,7 @@ source ./helpers.fish
 print "Configuring lldb"
 
 mkdir "$HOME/.lldb" > /dev/null 2>&1
-cp (dirname "$0")/lldb/.lldbinit "$HOME/.lldb/"
+cp (dirname "$0")/lldb/.lldbinit "$HOME/"
 
 print "Copying lldb-scripts"
 git clone https://github.com/lateralusd/lldb-scripts "$HOME/lldb-scripts"
