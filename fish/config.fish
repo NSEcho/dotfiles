68f9bb9ac02658e91df40a542dc298cd557b9bb7
NSEcho <xdaemonx@protonmail.ch>
2023-05-18 09:55:50 +0200

    fish: add vi key bindings

--- fish/config.fish
+++ fish/config.fish
@@ -3,4 +3,6 @@ if status is-interactive
     abbr -a !! --position anywhere --function last_history_item
     abbr -a litem --position anywhere --function last_item
     abbr -a gcl git clone
+
+    fish_vi_key_bindings
 end
