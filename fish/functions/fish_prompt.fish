ae52a365aaf1c37e1b5a342ff482e7a938e21010
NSEcho <xdaemonx@protonmail.ch>
2023-10-31 09:03:32 +0100

    update fish and brew

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,5 +1,5 @@
 function fish_prompt
-    printf '%snsecho%s %s|%s %s%s%s %s>%s ' (set_color -o red) (set_color normal) \
+    printf '%snsecho%s %s|%s %s%s%s %s>%s ' (set_color -o 5ce1e6) (set_color normal) \
            (set_color -o cyan) (set_color normal) \
            (set_color $fish_color_cwd) (prompt_pwd) (set_color normal) \
            (set_color -o cyan) (set_color normal)
