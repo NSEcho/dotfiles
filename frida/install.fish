a8b2e0dedb03c3f5aa0aa4f019e7ecae2fbd56ab
Erhad Husovic <erhadhusovic@Erhads-MacBook-Pro.local>
2023-10-31 13:09:06 +0100

    update frida install script

--- frida/install.fish
+++ frida/install.fish
@@ -2,8 +2,33 @@
 
 source ./helpers.fish
 
+set -l lib "/usr/local/lib"
+set -l inc "/usr/local/include"
+
+if ! test -d "$lib"
+    print "$lib does not exist; creating now"
+    sudo mkdir "$lib"
+end
+
+if ! test -d "$inc"
+    print "$inc does not exist; creating now"
+    sudo mkdir "$inc"
+end
+
 print "Downloading latest frida devkit"
 
-set -l type (machine)
-set -l latest (git ls-remote --tags --sort="v:refname" https://github.com/frida/frida.git | tail -n1 | cut -d '/' -f3)
-print "Downloading version $latest for $type"
+set -l mtype (machine)
+set -l latest (curl --silent "https://api.github.com/repos/frida/frida/releases/latest" | jq -r .tag_name)
+print "Downloading version $latest for $mtype"
+
+# https://github.com/frida/frida/releases/download/16.1.4/frida-core-devkit-16.1.4-macos-arm64e.tar.xz
+set -l url "https://github.com/frida/frida/releases/download/$latest/frida-core-devkit-$latest-macos-$mtype.tar.xz"
+wget $url -O - | tar --extract --xz
+
+print "Copying libfrida-core.a to $lib"
+sudo cp libfrida-core.a "$lib"
+print "Copying frida-core.h to $inc"
+sudo cp frida-core.h "$inc"
+
+print "Removing downloaded data"
+rm frida-core.h frida-core.gir frida-core-example.c libfrida-core.a
