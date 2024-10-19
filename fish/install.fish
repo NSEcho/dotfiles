ab23efe3e717cbc4b6cbf369e62b2c122d92108e
NSEcho <xdaemonx@protonmail.ch>
2024-10-19 23:28:20 +0200

    add fish theme

--- fish/install.fish
+++ fish/install.fish
@@ -3,6 +3,9 @@
 fish_add_path "$HOME/go/bin"
 set -Ux LSCOLORS gxfxcxdxbxegedabagacad
 
+# set theme
+fish_config theme choose "None"
+
 # install functions
 cp (dirname "$0")/fish/config.fish "$HOME/.config/fish"
 cp (dirname "$0")/fish/functions/* "$HOME/.config/fish/functions/"
